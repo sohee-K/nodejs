1ecfc46fe0aa7398eace8f6a8bf7442fe0335846
Sohee KWON <soheekwon@naver.com>
2020-09-25 01:47:38 +0900

    add node.js function to list

--- main.js
+++ main.js
@@ -3,22 +3,24 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    var dataFile = 'data';
-    fs.readdir(dataFile, (err, dataList) => {
-      dataList.forEach(data => {
-        console.log(data);
-      })
-    });
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
 
-    if (pathname === '/') {
-      fs.readFile(`data/${title}.txt`, 'utf8', (err, description) => {
-        if (title === undefined) {
-          title = 'Welcome';
+  if (pathname === '/') {
+    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+      if (title === undefined) {
+        title = 'Welcome';
+        description = "Hello, node.js!";
+      }
+
+      fs.readdir('data', (err, files) => {
+        var dataList = `<ul>`;
+        for (let i = 0; i < files.length; i++) {
+          dataList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
         }
+        dataList += `</ul>`;
 
         var template = `
         <!doctype html>
@@ -29,25 +31,21 @@ var app = http.createServer(function(request,response){
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
+          ${dataList}
           <h2>${title}</h2>
           ${description}
         </body>
         </html>
         `;
-  
+
         response.writeHead(200);
         response.end(template);
       });
-    } else {
-      response.writeHead(404);
-      response.end('Not Found');
-    }
-    
+    });
+  } else {
+    response.writeHead(404);
+    response.end('Not Found');
+  }    
 });
 app.listen(3000);
 // 파일 불러오기: response.end(fs.readFileSync(__dirname + _url));
