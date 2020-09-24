6dbf7ed5d2b039741efbae629688befd568382b8
Sohee KWON <soheekwon@naver.com>
2020-09-25 02:26:40 +0900

    add comment, arrange with functions

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
+function createTemplate(title, dataList, body) {
+  return `<!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${dataList}
+      ${body}
+    </body>
+    </html>`;
+}
 
-  if (pathname === '/') {
+function createList(fileList) {
+  let dataList = `<ul>`;
+        for (let i = 0; i < fileList.length; i++) {
+          dataList += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+        }
+        dataList += `</ul>`;
+  return dataList;
+}
+
+let app = http.createServer(function(request,response) {
+  let _url = request.url;
+  let queryData = url.parse(_url, true).query;
+  let pathname = url.parse(_url, true).pathname;
+  let title = queryData.id;
+
+  if (pathname === '/') { // 정상적으로 작동하는 페이지
     fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-      if (title === undefined) {
+      if (title === undefined) {  // index 페이지일 경우
         title = 'Welcome';
         description = "Hello, node.js!";
       }
 
-      fs.readdir('data', (err, files) => {
-        var dataList = `<ul>`;
-        for (let i = 0; i < files.length; i++) {
-          dataList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-        }
-        dataList += `</ul>`;
-
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${dataList}
-          <h2>${title}</h2>
-          ${description}
-        </body>
-        </html>
-        `;
+      fs.readdir('data', (err, fileList) => {
+        let dataList = createList(fileList);
+        let template = createTemplate(title, dataList, `<h2>${title}</h2>${description}`);
 
         response.writeHead(200);
         response.end(template);
       });
     });
-  } else {
+  } else {  // 오류 페이지
     response.writeHead(404);
     response.end('Not Found');
   }    
