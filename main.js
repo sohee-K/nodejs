4b0475b93febcaa0adba0aeba97ed405928cdf02
Sohee KWON <soheekwon@naver.com>
2020-09-25 19:02:53 +0900

    create form and submit

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const qs = require('querystring');
+
+function createForm() {
+  return `<form action="http://localhost:3000/create_process" method="post">
+    <p><input type=text name="title" placeholder="title"></p>
+    <p><textarea name="description" placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+  </form>`;
+}
 
 function createTemplate(title, dataList, body) {
   return `<!doctype html>
@@ -12,6 +21,7 @@ function createTemplate(title, dataList, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${dataList}
+      <a href="/create">Create Form</a>
       ${body}
     </body>
     </html>`;
@@ -26,32 +36,64 @@ function createList(fileList) {
   return dataList;
 }
 
-let app = http.createServer(function(request,response) {
-  let _url = request.url;
-  let queryData = url.parse(_url, true).query;
-  let pathname = url.parse(_url, true).pathname;
-  let title = queryData.id;
+function createApp() {
+  let app = http.createServer(function(request,response) {
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
+    let title = queryData.id;
 
-  if (pathname === '/') { // 정상적으로 작동하는 페이지
-    fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-      if (title === undefined) {  // index 페이지일 경우
-        title = 'Welcome';
-        description = "Hello, node.js!";
-      }
+    if (pathname === '/') { // 정상적으로 작동하는 페이지
+      fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+        if (title === undefined) {  // index 페이지일 경우
+          title = 'Welcome';
+          description = "Hello, node.js!";
+        }
 
-      fs.readdir('data', (err, fileList) => {
+        fs.readdir('data', 'utf8', (err, fileList) => {
+          let dataList = createList(fileList);
+          let template = createTemplate(title, dataList, `<h2>${title}</h2>${description}`);
+
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if (pathname === '/create') {
+      title = 'Web1 - Create';
+      fs.readdir('data', 'utf8', (err, fileList) => {
         let dataList = createList(fileList);
-        let template = createTemplate(title, dataList, `<h2>${title}</h2>${description}`);
+        let template = createTemplate(title, dataList, createForm());
 
         response.writeHead(200);
         response.end(template);
       });
-    });
-  } else {  // 오류 페이지
-    response.writeHead(404);
-    response.end('Not Found');
-  }    
-});
-app.listen(3000);
+    } else if (pathname === '/create_process') {
+      let body = '';
+      request.on('data', function (data) {
+        body += data;
+      });
+      request.on('end', function() {
+        let post = qs.parse(body);
+        let p_title = post.title;
+        let p_description = post.description;
+
+        fs.writeFile(`response/${p_title}`, p_description, 'utf8', (err) => {
+          response.writeHead(302, {Location: `/?id=${p_title}`});
+          response.end();
+        });
+      });
+    } else {  // 오류 페이지
+      response.writeHead(404);
+      response.end('Not Found');
+    }    
+  });
+  app.listen(3000);
+}
+
+function init() {
+  createApp();
+}
+
+init();
 // 파일 불러오기: response.end(fs.readFileSync(__dirname + _url));
 // 콘솔 입력: args = process.argv[2]
